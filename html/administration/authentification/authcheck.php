bf07603a12cf4a82ff659b8b70f40b586605ca24
philippeSimier <philaure@wanadoo.fr>
2021-05-02 23:04:34 +0200

    improve index.php

--- html/administration/authentification/authcheck.php
+++ html/administration/authentification/authcheck.php
@@ -3,41 +3,37 @@
 session_start();
 $_SESSION['request_uri'] = $_SERVER["REQUEST_URI"];
 
-function ErrorPersonnalise($errno, $errstr, $errfile, $errline){
+function ErrorPersonnalise($errno, $errstr, $errfile, $errline) {
     echo "Erreur numÃ©ro [$errno], ligne [$errline], du fichier $errfile :  $errstr";
-	die();
+    die();
 }
+
 set_error_handler("ErrorPersonnalise");
 
 
-if(!isset($_SESSION['last_access']) || !isset($_SESSION['ipaddr']) || !isset($_SESSION['login']) )
-{
-    header("Location: index.php" );
+if (!isset($_SESSION['last_access']) || !isset($_SESSION['ipaddr']) || !isset($_SESSION['login'])) {
+    header("Location: index.php");
     return;
 }
+// vérification du temps maxi pour une session (1 heure)
 
-// vÃ©rification du temps maxi pour une session (1 heure)
-
-if(time()-$_SESSION['last_access'] > 3600)
-{
+if (time() - $_SESSION['last_access'] > 3600) {
     unset($_SESSION['last_access']);
     unset($_SESSION['id']);
     unset($_SESSION['ipaddr']);
-	$_SESSION['erreur'] = "Your session has timed out. Please log in again.";
+    $_SESSION['erreur'] = "Your session has timed out. Please log in again.";
     header("Location: index.php");
     return;
 }
 
-// vÃ©rification de l'adresse IP du client elle ne doit pas changer
-if($_SERVER['REMOTE_ADDR']!=$_SESSION['ipaddr'])
-{
+// vérification de l'adresse IP du client elle ne doit pas changer
+if ($_SERVER['REMOTE_ADDR'] != $_SESSION['ipaddr']) {
     unset($_SESSION['last_access']);
     unset($_SESSION['id']);
     unset($_SESSION['ipaddr']);
-	$_SESSION['erreur'] = "IP address changed";
+    $_SESSION['erreur'] = "IP address changed";
     header("Location: index.php");
     return;
 }
 
-$_SESSION['last_access']=time();
-?>
+$_SESSION['last_access'] = time();
--- html/administration/blog.php
+++ html/administration/blog.php
@@ -13,7 +13,7 @@ use Aggregator\Support\Form;
 
 $bdd = Api::connexionBD(BASE, $_SESSION['time_zone']);
 
-//------------si des donnÃ©es  sont soumises on les enregistre dans la table data.blogs ---------
+//------------si des données  sont soumises on les enregistre dans la table data.blogs ---------
 if (!empty($_POST['envoyer'])) {
     if ($_SESSION['tokenCSRF'] === $_POST['tokenCSRF']) { // si le token est valide
         try {
@@ -22,7 +22,7 @@ if (!empty($_POST['envoyer'])) {
                         , $bdd->quote($_POST['thing_id'])
                         , $bdd->quote($_POST['title'])
                         , $bdd->quote($_POST['keyWord'])
-                        , $bdd->quote($_POST['date'].' '.$_POST['heure'])
+                        , $bdd->quote($_POST['date'] . ' ' . $_POST['heure'])
                         , $bdd->quote($_POST['comment'])
                         , $bdd->quote($_POST['status'])
                 );
@@ -33,7 +33,7 @@ if (!empty($_POST['envoyer'])) {
                         , $bdd->quote($_POST['thing_id'])
                         , $bdd->quote($_POST['title'])
                         , $bdd->quote($_POST['keyWord'])
-                        , $bdd->quote($_POST['date'].' '.$_POST['heure'])
+                        , $bdd->quote($_POST['date'] . ' ' . $_POST['heure'])
                         , $bdd->quote($_POST['comment'])
                         , $bdd->quote($_POST['status'])
                         , $_POST['id']
@@ -67,27 +67,29 @@ else {
             $blog = new stdClass();
             $blog->action = "insert";
             $blog->id = 0;
-			if (isset($_GET['thingId'])){ $blog->thing_id = $_GET['thingId']; }
-            else { $blog->thing_id = 0; }
+            if (isset($_GET['thingId'])) {
+                $blog->thing_id = $_GET['thingId'];
+            } else {
+                $blog->thing_id = 0;
+            }
             $blog->title = "";
             $blog->keyWord = "";
             $blog->visitDate = date("Y-m-d H:i:s");
             $blog->comment = "";
             $blog->status = "";
         }
-		
-		
+
         $dateTime = explode(" ", $blog->visitDate);
-		$blog->date = $dateTime[0];
-		$blog->heure = substr($dateTime[1],0,5);
-		
-		// CrÃ©ation du selectThing
+        $blog->date = $dateTime[0];
+        $blog->heure = substr($dateTime[1], 0, 5);
 
-        if ($_SESSION['droits'] > 1)
+        // Création du selectThing
+
+        if ($_SESSION['droits'] > 1) {
             $sql = "SELECT id,name FROM `things` ORDER BY id;";
-        else
+        } else {
             $sql = "SELECT id,name FROM `things` where user_id = {$_SESSION['id']} ORDER BY id;";
-
+        }
         $stmt = $bdd->query($sql);
 
         $selectThing = array();
@@ -102,43 +104,38 @@ else {
     function afficherMetaBlog($blog, $selectThing) {
 
         global $lang;
-        // CrÃ©ation du tokenCSRF
+        // Creation du tokenCSRF
         $tokenCSRF = STR::genererChaineAleatoire(32);
         $_SESSION['tokenCSRF'] = $tokenCSRF;
 
         echo Form::hidden('action', $blog->action);
         echo Form::hidden("tokenCSRF", $_SESSION["tokenCSRF"]);
-		echo Form::hidden("id", $blog->id);
+        echo Form::hidden("id", $blog->id);
 
-        $options = array('class' => 'form-control', 'readonly' => null);
-        echo Form::input('text', 'author', $_SESSION["login"], $options, 'Auteur');
-       
+        $optionsRO = array('class' => 'form-control', 'readonly' => null);
+        echo Form::input('text', 'author', $_SESSION["login"], $optionsRO, 'Auteur');
 
-        $options = array('class' => 'form-control', 'required'=>'required');
-        echo Form::input('text', 'title', $blog->title, $options, $lang['title']);
-		
-		$options = array('class' => 'form-control');
+
+        $optionsRE = array('class' => 'form-control', 'required' => 'required');
+        echo Form::input('text', 'title', $blog->title, $optionsRE, $lang['title']);
+
+        $options = array('class' => 'form-control');
         echo Form::input('text', 'keyWord', $blog->keyWord, $options, $lang['keyWord']);
-		
-		echo Form::input('date', 'date', $blog->date, $options, "Date");
-		echo Form::input('time', 'heure', $blog->heure, $options, "Heure");
+
+        echo Form::input('date', 'date', $blog->date, $options, "Date");
+        echo Form::input('time', 'heure', $blog->heure, $options, "Heure");
         echo Form::select("thing_id", $selectThing, $lang['thing'], $blog->thing_id);
         echo Form::select("status", $lang['sel_status'], $lang['status'], $blog->status);
     }
-	
-	function afficherFormBlog($blog){
-		
-		global $lang;
-		
-		echo '<textarea class="form-control" name="comment" id="comment" >';
-		echo $blog->comment;
-		echo '</textarea>';
-	
-	
-	}	
-	
-	
-	
+
+    function afficherFormBlog($blog) {
+
+        global $lang;
+
+        echo '<textarea class="form-control" name="comment" id="comment" >';
+        echo $blog->comment;
+        echo '</textarea>';
+    }
 
 }
 ?>
@@ -149,92 +146,90 @@ else {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <title><?= $lang['blog'] ?> - Aggregator</title>
-        
-		
-		<link rel="stylesheet" href="../css/ruche.css" />
-		
-		<script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-		
-		<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
-		<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
-		
-		<link rel="stylesheet" href="../css/jquery-confirm.min.css" />
-		<script src="../scripts/jquery-confirm.min.js"></script>	
-		
-		<script>
-			$(document).ready(function() {
-				$('#comment').summernote({
-				    toolbar: [
-					    ['style', ['style','bold', 'italic', 'underline', 'clear']],
-						['font', ['superscript', 'subscript']],
-						['fontsize', ['fontsize']],
-						['color', ['color']],
-						['para', ['ul', 'ol', 'paragraph']],
-						['table', ['table']],
-						['insert', ['link','picture','video']],
-						['view', ['codeview']],
-				    ],
-				    height: 450,
-					maxHeight: 650,
-					focus: true ,
-					placeholder: 'write here...',
-				    codemirror: { 
-					    theme: 'monokai'
-				    }
-				});
-				
-				$("#ecrire").submit(function (e) {
-					console.log( "click", this);
-					if ($('#comment').summernote('isEmpty')) {
-						$.alert({
-							title: 'Alert!',
-							content: "L'article est vide"
-						});
-						e.preventDefault();
-						
-					}
-					
-				});	
-				
-			});
-		</script>
-		
+
+        <link rel="stylesheet" href="../css/ruche.css" />
+
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+        <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
+
+        <link rel="stylesheet" href="../css/jquery-confirm.min.css" />
+        <script src="../scripts/jquery-confirm.min.js"></script>	
+
+        <script>
+            $(document).ready(function () {
+                $('#comment').summernote({
+                    toolbar: [
+                        ['style', ['style', 'bold', 'italic', 'underline', 'clear']],
+                        ['font', ['superscript', 'subscript']],
+                        ['fontsize', ['fontsize']],
+                        ['color', ['color']],
+                        ['para', ['ul', 'ol', 'paragraph']],
+                        ['table', ['table']],
+                        ['insert', ['link', 'picture', 'video']],
+                        ['view', ['codeview']],
+                    ],
+                    height: 450,
+                    maxHeight: 650,
+                    focus: true,
+                    placeholder: 'write here...',
+                    codemirror: {
+                        theme: 'monokai'
+                    }
+                });
+
+                $("#ecrire").submit(function (e) {
+                    console.log("click", this);
+                    if ($('#comment').summernote('isEmpty')) {
+                        $.alert({
+                            title: 'Alert!',
+                            content: "L'article est vide"
+                        });
+                        e.preventDefault();
+
+                    }
+                });
+            });
+        </script>
+
     </head>
     <body>
 
-<?php require_once '../menu.php'; ?>
+        <?php require_once '../menu.php'; ?>
 
         <div class="container-fluid" style="padding-top: 65px;">
             <form class="form-horizontal" method="post" action="<?= $_SERVER['SCRIPT_NAME'] ?>" name="ecrire" id="ecrire">
-				<div class="row">
+                <div class="row">
 
-					<div class="col-md-4 col-sm-12 col-12">
-						<div class="popin">
-							
-								<?php afficherMetaBlog($blog,$selectThing ); ?>
+                    <div class="col-md-4 col-sm-12 col-12">
+                        <div class="popin">
 
-								<div class="form-group">
-									</br>
-									<button id="envoyer" type="submit" class="btn btn-primary" value="Valider" name="envoyer" > <?= $lang['Apply'] ?></button>
-									<a  class="btn btn-info" role="button" href="../blogs?id=<?= $blog->thing_id ?>"><?= $lang['Cancel'] ?></a>
-								</div>	
-							
-						</div>
-					</div>
+                            <?php afficherMetaBlog($blog, $selectThing); ?>
 
-					<div class="col-md-8 col-sm-12 col-12">
-						<div class="popin">
-								<?php afficherFormBlog( $blog ); ?>
-						</div>
-					</div>
-				
-				</div>	
-			</form>
-<?php require_once '../piedDePage.php'; ?>
+                            <div class="form-group">
+                                </br>
+                                <button id="envoyer" type="submit" class="btn btn-primary" value="Valider" name="envoyer" > <?= $lang['Apply'] ?></button>
+                                <a  class="btn btn-info" role="button" href="../blogs?id=<?= $blog->thing_id ?>"><?= $lang['Cancel'] ?></a>
+                            </div>	
+
+                        </div>
+                    </div>
+
+                    <div class="col-md-8 col-sm-12 col-12">
+                        <div class="popin">
+                            <?php afficherFormBlog($blog); ?>
+                        </div>
+                    </div>
+
+                </div>	
+            </form>
+            <?php require_once '../piedDePage.php'; ?>
         </div>
 
     </body>
